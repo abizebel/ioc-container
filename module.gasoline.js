b0219718914a3e90c86dd5bcc31d56c41b62c7c5
Abbas Hosseini <abizebel@gmail.com>
2018-07-31 21:45:57 +0430

    Delete module.gasoline.js

--- module.gasoline.js
+++
@@ -1,7 +0,0 @@
-Ioc.define('gasoline', function(){
-    return {
-        fill : function () {
-            document.body.innerHTML = 'person take a taxi and taxi fill the gasoline then go to destination'
-        }
-    }
-})
