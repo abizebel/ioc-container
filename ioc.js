ddbe790c4ad0c4c2684418bd36c5beb9c833e93d
Abbas Hosseini <abizebel@gmail.com>
2018-07-31 21:46:10 +0430

    Update ioc.js

--- ioc.js
+++ ioc.js
@@ -1,161 +1,140 @@
 /*******************************************************************************************
- * @ Name : Rcore ioc container
+ * @ Name : Angular dependency injection
  * @ Author : Abbas Hosseini
- * @ Description : a simple library that provides you a modular JavaScript 
+ * @ Description : A simple library that support dependency injection with angular syntax
  * @ Version : 1.0.0
- * @ Last update : Tuesday - 2018 29 May
+ * @ Last update : Thursday - 2018 08 February
  ******************************************************************************************/
 
+var core = (() =>{
 
-var Ioc = (function () {
-    var containers = {};
-    var cache = {};
-    /* 
-     *@ injector tasks :
-     *@ 1- annotate : find && instnaciate injection modules 
-     *@ 2- check injection : check if factory has injectables then instnaciate it again with injections arguments
+    /**
+     * IoC container symbol
+     * 
+     * @type {Symbol}
+     * @private
+     * @description : Symbols is a good replacement for 
+     * strings or integers as class/module constants
      */
-    var injector = {
-         /*
-         * @param {String} name is module name
-         * @returns {Array} 
-         * @description : show list of injected moduleNames
-         */
-        getInjectList : function(name){
-            return containers[name].injects
-        },
-        /*
-         * @param {String} name is module name
-         * @returns {Object} return a instance with injection mdoules
-         * @description : create a factory instance of module
-         */
-        instanciate: function (name) {
-            var factory = containers[name].factory,
-                injects = containers[name].injects;
-            return factory.apply(null, injector.annotate(injects));
-        },
-        /*
-         * @param {Array} fnArgs is injection arguments
-         * @returns {Array} instanciate all injected modules as array
-         * @description : with annotation process injector knows what modules to inject into the function
-         */
-        annotate: function (fnArgs) {
-            var injectedModules = [];
-            fnArgs.forEach(function (arg) {
-                var injectedModuleName = arg.trim();
-                var injectedModule = injector.processModule(injectedModuleName);
-                injectedModules.push(injectedModule);
+    const _container = Symbol('container');
 
-            });
-            return injectedModules;
-        },
-        /*
+    /**
+     * Cache symbol
+     * @type {Symbol}
+     * @private
+     */
+    const _cache = Symbol('cache');
+
+    /**
+     * IoC container class
+     * 
+     * @class {IOC}
+     * @description : IoC container (or DI) is a design pattern that 
+     * creates objects based on request and inject them when required
+     */
+    class IOC {
+        constructor() {
+            this[_container] = new Map();
+            this[_cache] = new Map();
+        }
+        /**
+         * Get the shared container
+         * 
+         * @returns {Map<string, object>}
+         */
+        get container() {
+            return this[_container];
+        }
+
+        /**
+         * Get the shared container
+         * 
+         * @returns {Map<string, object>}
+         */
+        get cache() {
+            return this[_cache];
+        }
+
+        /**
+         * Register a module to IoC container
+         * 
          * @param {String} name is the module name
-         * @returns {Object} return a instance of requested module factory
-         * @description : check chached & injactables factories then return module
+         * @param {Function} factory is a factory function 
+         * @returns return true when defining a module that was not previously defined
          */
-        processModule: function (name) {
-            if (!containers[name]) {
-                return
-            }
-            if (isCached(name)) {
-                return getCache(name)
-            } else {
-                var factory = containers[name].factory;
-                if (injector.hasInject(name)) {
-                    //if factory has injectables should instnaciate again with injections arguments
-                    setCache(name, injector.instanciate(name))
-                } else {
-                    setCache(name, factory())
-                }
-                return getCache(name);
-            }
-        },
-        /*
-         * @param {Function}
-         * @returns {Boolean} 
-         * @description : check if inject module has injection 
-         */
-        hasInject: function (name) {
-            return (containers[name].injects.length != 0)
-        }
+        register(name, factory) {
 
-    }
-    /*
-     * @param {String} name is the module name
-     * @param {Function} factory is a factory function 
-     * @description : add a new module in ioc container
-     */
-    function define(name, factory) {
-        if (!containers.hasOwnProperty(name)) {
-            containers[name] = {
+            if (this.container.has(name))
+                throw new Error(`adding ${name} to container failed, because its already exists.`)
+
+            this.container.set(name, {
                 factory: factory,
-                injects: extractArgs(factory),
+                dependencies: this.extractArgs(factory) || [],
+            });
+        }
+
+        /**
+         * Resolve a injected module
+         * 
+         * @param {String} name is the name of requested module
+         * @returns {Object} 
+         */
+        resolve(name) {
+
+            if (this.cache.has(name))
+                return this.cache.get(name);
+
+            if (!this.container.has(name)) return undefined;
+
+            let self = this,
+                factory = this.container.get(name).factory,
+                dependencies = this.container.get(name).dependencies;
+
+            if (dependencies.length == 0) {
+                var instance = factory();
+                this.cache.set(name, instance);
             }
+
+            var parameters = [];
+            dependencies.forEach(dependency => parameters.push(self.resolve(dependency)));
+
+            var instance = factory.apply(null, parameters);
+            this.cache.set(name, instance);
+
+            return instance;
+        }
+
+        /**
+         * Extract arguments of function
+         * 
+         * @param {Function} fn is a factory function
+         * @returns {Array} args is list of function arguments
+         */
+        extractArgs(fn) {
+            let ARROW_ARG = /^\s*[^\(]*\(\s*([^\)=>]*)\)/,
+                FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
+                fnText = fn.toString(),
+                regResult = fnText.match(FN_ARGS) || fnText.match(ARROW_ARG),
+                args = regResult[1].split(',').map((item) => {
+                    return item.trim();
+                })
+            //fix split string ''
+            args = (args[0] == '') ? [] : args;
+            return args;
         }
     }
 
-    /*
-     * @param {String}
-     * @returns {Object} 
-     * @description : get requested module
-     */
-    function getModule(name) {
-        return injector.instanciate(name)
-    }
 
-    /*
-     * @param {Function} 
-     * @returns {Array} 
-     * @description : extract arguments of function
-     */
-    function extractArgs(func) {
-        var FN_ARGS_REG = /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
-            args = func.toString().match(FN_ARGS_REG)[1].split(',');
-        if (args[0] == '') {
-            args = []
-        }
-        return args;
-    }
+    var ioc = new IOC();
 
-    /*
-     * @param {String} 
-     * @returns {Boolean} 
-     * @description : check cache already hast module instance or not
-     */
-    function isCached(name) {
-        if (cache.hasOwnProperty(name)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /*
-     * @param {String}
-     * @returns {Object} 
-     * @description : get a module instance from cache
-     */
-    function getCache(name) {
-        return cache[name];
-    }
-
-    /*
-     * @param {String} 
-     * @param {Object} 
-     * @returns {Nothing} 
-     * @description : set instance of a module in cache
-     */
-    function setCache(name, instance) {
-        cache[name] = instance
-    }
-
-    //PUBLIC API
+    //Public Interface
     return {
-        define: define,
-        service: define,
-        getModule: getModule,
-        getInjects : injector.getInjectList
+        define : (moduleName, fn) =>{
+            ioc.register(moduleName, fn)
+        },
+        require: (moduleName) => {
+            return ioc.resolve(moduleName)
+        },
 
     }
-})();
+})()
